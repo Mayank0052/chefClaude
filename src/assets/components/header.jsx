16bf4a3ff5b87dd953c057a9cafcd3817fc460d0
Neeraj.Sh-16 <shneeraj.2619@gmail.com>
2025-02-02 11:28:46 +0530

    Updated icon and image urls

--- src/assets/components/header.jsx
+++ src/assets/components/header.jsx
@@ -1,7 +1,7 @@
 export default function headerFunction(){
     return(
         <header className="header">
-            <img src="https://drive.google.com/file/d/1mxoGm8R-9nDZyC5TDNPQOmukzQru6RYE/view?usp=sharing" alt="chef-icon" />
+            <img src="/images/chef-claude-icon.png" alt="chef-icon" />
             <h1>Chef Claude</h1>
         </header>
     )
